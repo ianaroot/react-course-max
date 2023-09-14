2ec30e59c29821ebd71169762adb229633dc8118
ianaroot <ianaroot@gmail.com>
2023-09-14 13:14:46 -0700

    form data submitting, not saving results

---
+++ src/components/Expenses/ExpenseForm.js
@@ -0,0 +1,50 @@
+import './ExpenseForm.css';
+import React, { useState } from 'react';
+
+const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState('')
+  const [enteredAmount, setEnteredAmount] = useState(0)
+  const [enteredDate, setEnteredDate] = useState('')
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    };
+    setEnteredAmount('');
+    setEnteredTitle('');
+    setEnteredDate('');
+    console.log(expenseData)
+  };
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value)
+  };
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value)
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value)
+  };
+
+  return( <form onSubmit={submitHandler}>
+    <div className='new-expense__controls'>
+      <div className='new-expense__control'>
+        <label>Title</label>
+        <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
+        <label>Amount</label>
+        <input type='number' min='0.01' step='0.01' value={enteredAmount} onChange={amountChangeHandler} />
+        <label>Date</label>
+        <input type='date' min='2019-01-01' max='2022-12-31' value={enteredDate} onChange={dateChangeHandler} />
+      </div>
+    </div>
+    <div className='new-expense-actions'>
+      <button type='submit'>Add Expense</button>
+    </div>
+  </form>)
+
+};
+
+export default ExpenseForm
