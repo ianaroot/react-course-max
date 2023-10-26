2de6189edf65c75784dfc7d32e61138efcfa4276
ianaroot <ianaroot@gmail.com>
2023-10-25 17:02:00 -0700

    null filter message

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -18,19 +18,24 @@ function Expenses(props) {
 
   const filteredExpenses = props.items.filter((expense) => expense.date.getFullYear().toString() === yearFilter);
 
+  let expenseContent = <p>No expenses found.</p>;
+
+  if ( filteredExpenses.length > 0 ){
+    expenseContent = filteredExpenses.map(expense =>  (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
+
   return(
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={yearFilter} onChangeYearFilter={yearFilterHandler}/>
-        {filteredExpenses.map(expense =>  (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))
-      }
+        { expenseContent}
         <p>Data for years {yearFilterText} is hidden</p>
 
       </Card>
